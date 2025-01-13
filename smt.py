509bd17cc4fe69693861c44d080d6b80655ab7e7
jana <jnikolovskaa@gmail.com>
2025-01-13 21:00:07 +0100

    [smt][utils][results] refactor smt code and update utils, push new results, [Dockerfile] update so it copies all folder not just specific files

--- smt.py
+++ smt.py
@@ -2,9 +2,34 @@ from z3 import *
 from utils.utils import *
 
 def round_trip_distance(item, depot, distance_matrix):
+    """
+        Computes the round-trip distance for an item from the depot and back.
+
+        Args:
+            item (int): The index of the item.
+            depot (int): The index of the depot.
+            distance_matrix (list of list of ints): A 2D matrix representing the distances between points.
+
+        Returns:
+            int: The sum of the distance from depot to item and back.
+    """
     return distance_matrix[depot][item] + distance_matrix[item][depot]
 
 def extract_ordered_route(model, courier_id, num_items,x, y, item_encodings):
+    """
+        Extracts the ordered route for a specific courier from the Z3 model.
+
+        Args:
+            model (z3.Model): The Z3 model containing the decision variables.
+            courier_id (int): The index of the courier.
+            num_items (int): The total number of items.
+            x (list of list of z3.Bool): The assignment matrix for items to couriers.
+            y (list of list of list of z3.Bool): The travel path matrix for couriers.
+            item_encodings (dict): A dictionary mapping item indices to encodings.
+
+        Returns:
+            list: A list of item encodings in the order they are assigned to the courier.
+    """
     ordered_route = []
     assigned_items = [j for j in range(num_items) if is_true(model.evaluate(x[courier_id][j]))]
     current_location = num_items
@@ -20,6 +45,15 @@ def extract_ordered_route(model, courier_id, num_items,x, y, item_encodings):
     return
 
 def compute_larger_bound(matrix):
+    """
+       Computes a larger bound for the optimization problem by selecting the maximum values from the matrix.
+
+       Args:
+           matrix (list of list of ints): A 2D matrix representing the distances or costs.
+
+       Returns:
+           int: The sum of the largest values from each row of the matrix.
+    """
     used_columns = set()
     total_sum = 0
 
@@ -33,6 +67,17 @@ def compute_larger_bound(matrix):
     return total_sum
 
 def compute_lower_bound(depot, num_items, distance_matrix):
+    """
+        Computes a lower bound for the optimization problem based on the round-trip distances from the depot.
+
+        Args:
+            depot (int): The index of the depot.
+            num_items (int): The total number of items.
+            distance_matrix (list of list of ints): A 2D matrix representing the distances between points.
+
+        Returns:
+            int: The maximum round-trip distance for any item from the depot.
+    """
     max_distance = 0
     for item in range(num_items):
         # Calculate the round-trip distance for each item
@@ -41,7 +86,43 @@ def compute_lower_bound(depot, num_items, distance_matrix):
 
     return max_distance
 
-def solve_mcp_z3(num_couriers, num_items, load_limits, item_sizes, int_distance_matrix, verbose=True):
+def create_variables(num_couriers, num_items):
+    x = [[Bool(f'x_{i}_{j}') for j in range(num_items)] for i in range(num_couriers)]
+    y = [[[Bool(f'y_{i}_{p}_{q}') for q in range(num_items + 1)] for p in range(num_items + 1)] for i in range(num_couriers)]
+    order = [[Int(f"order_{i}_{j}") for j in range(num_items)] for i in range(num_couriers)]
+    d = [Int(f'd_{i}') for i in range(num_couriers)]
+    return x, y, order, d
+
+def set_bounds(int_distance_matrix, num_items, opt, max_distance):
+    upper_bound = compute_larger_bound(int_distance_matrix)
+    lower_bound = compute_lower_bound(num_items, num_items, int_distance_matrix)
+    opt.add(max_distance <= upper_bound)
+    opt.add(max_distance >= lower_bound)
+    return upper_bound, lower_bound
+
+def solve_mcp_z3(num_couriers,
+                 num_items,
+                 load_limits,
+                 item_sizes,
+                 int_distance_matrix,
+                 res_path,
+                 inst_id,
+                 solver_name,
+                 verbose=True):
+    """
+        Solves the Multi-Courier Problem (MCP) using the Z3 solver.
+
+        Args:
+            num_couriers (int): The number of couriers.
+            num_items (int): The number of items to be delivered.
+            load_limits (list of ints): The load limits for each courier.
+            item_sizes (list of ints): The sizes of each item.
+            int_distance_matrix (list of list of ints): The distance matrix between items and depot.
+            verbose (bool): Flag to enable detailed logging.
+
+        Returns:
+            dict: A dictionary containing the execution time, optimal solution status, objective value, and the solution routes.
+    """
     start_time = time.time()
     timeout = 300
     current_datetime()
@@ -57,62 +138,43 @@ def solve_mcp_z3(num_couriers, num_items, load_limits, item_sizes, int_distance_
                        for p in range(num_items + 1)]
 
     # Decision Variables:
-    # x[i][j] = 1 if courier i is assigned to item j, 0 otherwise
-    x = [[Bool(f'x_{i}_{j}') for j in range(num_items)] for i in range(num_couriers)]
-
-    # y[i][p][q] = 1 if courier i travels from point p to point q, 0 otherwise
-    y = [[[Bool(f'y_{i}_{p}_{q}') for q in range(num_items + 1)]
-          for p in range(num_items + 1)] for i in range(num_couriers)]
-
-    order = [[Int(f"order_{i}_{j}") for j in range(num_items)] for i in range(num_couriers)]
-
-    # d[i] = total distance traveled by courier i
-    d = [Int(f'd_{i}') for i in range(num_couriers)]
+    x, y, order, d = create_variables(num_couriers, num_items)
 
     # max_distance = the maximum distance traveled by any courier
     max_distance = Int('max_distance')
 
     # Initialize lower bound
-    depot = num_items
-    upper_bound = compute_larger_bound(int_distance_matrix)
-    lower_bound = compute_lower_bound(depot, num_items, int_distance_matrix)
-    opt.add(max_distance <= upper_bound)
-    opt.add(max_distance >= lower_bound)
-    log(f"Bounds set. Upper: {upper_bound}, Lower: {lower_bound}", verbose)
+    upper_bound, lower_bound = set_bounds(int_distance_matrix, num_items, opt, max_distance)
+    log(f"Bounds set. Upper: {upper_bound}, Lower: {lower_bound}", True)
 
+    log("Adding constraints...", verbose)
     # Breaking courier symmetry - Enforce order by distance traveled
     for i in range(num_couriers - 1):
         opt.add(d[i] <= d[i + 1])
 
-    check_elapsed_time(start_time, timeout)
-
     # Constraints:
-    log("Adding constraints...",verbose)
 
-    # Each item must be assigned to exactly one courier DEMAND FULFILLMENT
+    # Each item must be assigned to exactly one courier (DEMAND FULFILLMENT)
     for j in range(num_items):
         opt.add(Sum([If(x[i][j], 1, 0) for i in range(num_couriers)]) == 1)
 
-    check_elapsed_time(start_time, timeout)
-
-    # Iterate over couriers and items
+    # #TODO
+    # Maintaining a valid path structure when number of items per courier > 1
     for i in range(num_couriers):
         for k in range(num_items):
-            # Create the sum of x[i][j] over all j items
+            # how many items the courier i is assigned
             sum_x = Sum([If(x[i][j], 1, 0) for j in range(num_items)])
 
-            # Check if sum_x > 1, then add the condition that you can't have both transitions
+            # Check if sum_x > 1, then add the condition that you can't have both transitions (at least one more item before going back to depot)
             opt.add(Implies(sum_x > 1,
                             Not(And(y[i][num_items][k], y[i][k][num_items]))))
 
-    # Each courier's load must not exceed their capacity CAPACITY CONSTRAINT
+    # Each courier's load must not exceed their capacity (CAPACITY CONSTRAINT)
     for i in range(num_couriers):
         # Add the load constraint for each courier
         opt.add(Sum([If(x[i][j], item_sizes[j], 0) for j in range(num_items)]) <= load_limits[i])
 
-    check_elapsed_time(start_time, timeout)
-
-    # Early Exclusion of Unusable Couriers
+    # Early Exclusion of items that cannot be delivered by certain couriers based on their load limits
     for i in range(num_couriers):
         for j in range(num_items):
             if item_sizes[j] > load_limits[i]:
@@ -120,35 +182,32 @@ def solve_mcp_z3(num_couriers, num_items, load_limits, item_sizes, int_distance_
                 for k in range(j + 1, num_items):
                     opt.add(Not(x[i][k]))
 
-    check_elapsed_time(start_time, timeout)
-
     # Ensure each courier has at least one item
     for i in range(num_couriers):
         opt.add(Sum([x[i][j] for j in range(num_items)]) >= 1)
-
-    check_elapsed_time(start_time, timeout)
 
     #Route Continuity
     for i in range(num_couriers):
         for p in range(num_items):
             opt.add(Not(y[i][p][p]))  # Ensure no self-loop (y[i][p][p] = 0)
-            for q in range(p + 1, num_items):  # Only consider pairs where p < q to avoid duplication
+            for q in range(p + 1, num_items):  #Ensures no reverse transitions
                 opt.add(Implies(y[i][p][q], Not(y[i][q][p])))
 
     for i in range(num_couriers):
         for p in range(num_items):
+            #connecting x variable with order variable
             opt.add(Implies(Not(x[i][p]), order[i][p] < 0))
             opt.add(Implies(x[i][p], order[i][p] > 0))
             for q in range(num_items):
                 # If there is a transition from p to q, enforce order[p] < order[q]
                 opt.add(Implies(y[i][p][q], order[i][p] < order[i][q]))
 
+    #uniqueness of order variable
     for i in range(num_couriers):
         opt.add(Distinct([order[i][p] for p in range(num_items)]))
 
-    check_elapsed_time(start_time, timeout)
-
     # Preventing redundant "revisits" to a location by ensuring only allowed transitions
+    # connection between x variable and y variable
     for i in range(num_couriers):
         for j in range(num_items):
             opt.add(Sum([If(y[i][p][j], 1, 0) for p in range(num_items + 1)]) == If(x[i][j], 1, 0))
@@ -156,8 +215,6 @@ def solve_mcp_z3(num_couriers, num_items, load_limits, item_sizes, int_distance_
         opt.add(Not(y[i][num_items][num_items]))
         opt.add(Sum([If(y[i][p][num_items], 1, 0) for p in range(num_items + 1)]) == 1)
         opt.add(Sum([If(y[i][num_items][q], 1, 0) for q in range(num_items + 1)]) == 1)
-
-    check_elapsed_time(start_time, timeout)
 
     # Calculate total distance for each courier
     for i in range(num_couriers):
@@ -184,7 +241,6 @@ def solve_mcp_z3(num_couriers, num_items, load_limits, item_sizes, int_distance_
 
     # Set a timeout (in milliseconds)
     time_left = max(1,timeout - int(time.time() - start_time))
-    opt.set("timeout", time_left * 1000)
     log(f"Time left for solution: {time_left}",verbose)
     current_datetime()
 
@@ -224,14 +280,40 @@ def solve_mcp_z3(num_couriers, num_items, load_limits, item_sizes, int_distance_
         "obj": obj if obj is not None else 0,  # Set to 0 if no objective found
         "sol":  [routes[i] for i in sorted_indices_load]
     }
-
+    save_results(res_path, inst_id, solver_name, result)
     return result
 
+
 if __name__ == "__main__":
-    args = sys.argv
-    log('SMT solver started')
-    inst_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),args[1])
-    os.makedirs(args[2], exist_ok=True)
-    res_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),args[2])
-    process_instances_input(inst_path,res_path,solve_mcp_z3,"SMT")
-    print('SAT solver done')
+    # Define and parse the arguments
+    parser = argparse.ArgumentParser(description="A simple SMT solver script with customizable arguments.")
+    parser.add_argument("--instances", "-i", required=True, help="Path to the folder containing instance files.")
+    parser.add_argument("--results", "-r", required=True, help="Path to the base folder where results will be stored.")
+    parser.add_argument("--folder-name", "-f", default="SMT",
+                        help="Name of the subfolder to store results (default: SMT).")
+    parser.add_argument("--selected", "-s", default="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21",
+                        help="An integer or a list of integers specifying selected instances "
+                             "(default: \"1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21\").")
+
+    args = parser.parse_args()
+
+    # Parse and handle the selected_instances argument
+    if args.selected.isdigit():
+        selected_instances = [int(args.selected)]
+    else:
+        selected_instances = list(map(int, args.selected.strip("[]").split(",")))
+
+    log("SMT solver started")
+
+    # Paths
+    inst_path = os.path.abspath(args.instances)
+    res_path = os.path.join(os.path.abspath(args.results), args.folder_name)
+
+    # Create the results folder if it doesn't exist
+    os.makedirs(res_path, exist_ok=True)
+
+    log(f"Selected instances: {selected_instances}")
+    # Process the instances
+    process_instances_input(inst_path, res_path, selected_instances,args.folder_name, solve_mcp_z3, "SMT")
+
+    log("SMT solver done")
--- utils/utils.py
+++ utils/utils.py
@@ -4,15 +4,48 @@ import json
 from datetime import datetime
 import time
 import argparse
+import multiprocessing
+import sys
+import signal
+
+def signal_handler(sig, frame):
+    print("Terminating processes and exiting...")
+    for p in multiprocessing.active_children():
+        p.terminate()
+    sys.exit(0)
 
 def current_datetime():
+    """ Prints the current date and time in the format YYYY-MM-DD HH:MM:SS """
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 def log(message, verbose=True):
+    """ Logs a message with the option to control verbosity.
+
+        Args:
+            message (str): The message to be logged.
+            verbose (bool): If True, the message is printed to the console.
+    """
     if verbose:
         print(f"[LOG] {message}")
 
+def save_results(res_path, inst_id, solver_name, result_data):
+    file_path = os.path.join(res_path, f"{inst_id}.json")
+    log(file_path)
+    with open(file_path, "w") as f:
+        json.dump({solver_name: result_data}, f, indent=4)
+    log(f"JSON data saved to {file_path}")
+
 def check_elapsed_time(start_time,timeout,print_time=False):
+    """ Checks if the elapsed time has exceeded a specified timeout.
+
+        Args:
+            start_time (float): The time at which the process started.
+            timeout (float): The timeout threshold in seconds.
+            print_time (bool): If True, the elapsed time is printed.
+
+        Returns:
+            bool: True if elapsed time exceeds timeout, otherwise False.
+    """
     elapsed_time = time.time() - start_time
     if print_time:
         log(f"elapsed time: {round(elapsed_time,2)}")
@@ -20,6 +53,20 @@ def check_elapsed_time(start_time,timeout,print_time=False):
 
 # Function to read the .dat file and store first four lines
 def read_dat_file(file_path, print_summary = False):
+    """ Reads a .dat file and extracts the first four lines as well as the rest of the data.
+
+        Args:
+            file_path (str): Path to the .dat file to be read.
+            print_summary (bool): If True, a summary of the contents is printed.
+
+        Returns:
+            tuple: A tuple containing:
+                - m (int): Number of couriers.
+                - n (int): Number of items.
+                - load_sizes (list): List of load sizes for each courier.
+                - item_sizes (list): List of item sizes.
+                - data (np.array): The distance matrix as a NumPy array.
+    """
     with open(file_path, 'r') as file:
         # Read the first four lines
         m = int(file.readline().strip())  # First line
@@ -45,6 +92,22 @@ def read_dat_file(file_path, print_summary = False):
     return m, n, load_sizes, item_sizes, np.array(data)
 
 def encode_input(item_sizes, load_limits , distance_matrix, sort_items, sort_loads):
+    """ Encodes the input by sorting the items and couriers based on specified flags and returns updated input data.
+
+        Args:
+            item_sizes (list): A list of item sizes.
+            load_limits (list): A list of courier load limits.
+            distance_matrix (list of lists): The distance matrix between items and couriers.
+            sort_items (bool): If True, sorts the items based on their size.
+            sort_loads (bool): If True, sorts couriers based on their load limits.
+
+        Returns:
+            tuple: A tuple containing:
+                - item_sizes (list): The sorted item sizes.
+                - load_limits (list): The sorted load limits.
+                - distance_matrix (list of lists): The updated distance matrix after sorting.
+                - (item_encodings, sorted_indices_load): A tuple with item encodings and sorted load indices.
+    """
     if sort_items:
         # Sort the items
         sorted_indices_item = sorted(range(len(item_sizes)), key=lambda k: item_sizes[k])
@@ -62,22 +125,44 @@ def encode_input(item_sizes, load_limits , distance_matrix, sort_items, sort_loa
 
         return item_sizes, load_limits , distance_matrix, (item_encodings,sorted_indices_load)
 
-def process_instances_input(inst_path, res_path, solver_function,solver_name):
+def process_instances_input(inst_path, res_path, selected_instances, folder_name, solver_function, solver_name):
+    """ Processes instances from a specified input folder and saves the results to the output folder.
+
+        Args:
+            inst_path (str): Path to the folder containing instance files.
+            res_path (str): Path to the folder where results will be stored.
+            selected_instances (list): List of instance IDs to process.
+            folder_name (str): Name of the subfolder in which results will be saved.
+            solver_function (function): The solver function to process each instance.
+            solver_name (str): The name of the solver to be included in the output file.
+    """
+    signal.signal(signal.SIGINT, signal_handler)  # Catch Ctrl+C
     for filename in os.listdir(inst_path):
         inst_file = os.path.join(inst_path, filename)
         inst_id = int(filename[4:-4])
-        n_couriers, n_items, capacity, sizes, dist_matrix = read_dat_file(inst_file)
-        log(f'\tLoaded input instance {filename}')
-        result = solver_function(n_couriers, n_items, capacity, sizes, dist_matrix)
-        file_path = os.path.join(res_path, f"{inst_id}.json")
+        if inst_id in selected_instances:
+            n_couriers, n_items, capacity, sizes, dist_matrix = read_dat_file(inst_file)
+            log(f'\tLoaded input instance {filename}')
+            process = multiprocessing.Process(
+                target=solver_function,
+                args=(n_couriers, n_items, capacity, sizes, dist_matrix, res_path, inst_id, solver_name)
+            )
+            process.start()
+            process.join(timeout=300)
+            if process.is_alive():
+                log("Timeout reached, terminating!")
+                process.terminate()  # Forcefully terminate the process
+                process.join()  # Ensure cleanup
+                result = {
+                    "time": 300,
+                    "optimal": False,
+                    "obj": 0,  # Set to 0 if no objective found
+                    "sol": []
+                }
 
-        # Save the JSON data to the file
-        with open(file_path, "w") as json_file:
-            json.dump({solver_name: result}, json_file, indent=4)
+                save_results(res_path, inst_id, solver_name, result)
 
-        log(f"JSON data saved to {file_path}")
-
-def parse_args(args):
+def parse_args(args):  #ask cico if he uses it??
     def parse_instances(value):
         # Check if the value is "all" or a comma-separated list
         if value.lower() == "all":
